8f2e7a4d9ab5dbe3b0a7ce5ee3dc24bb45574051
m4md24 <m4md24@gmail.com>
2025-12-24 00:56:02 +0200

    Enhance (SnackBars) Concept Code

--- app/src/main/java/barmajaa/m4md24/learnjetpackcompose/learn/components/snack_bars/SnackBars.kt
+++ app/src/main/java/barmajaa/m4md24/learnjetpackcompose/learn/components/snack_bars/SnackBars.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.HorizontalRule
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import barmajaa.m4md24.learnjetpackcompose.learn.components.snack_bars.ui.PreviewUI
 import kotlinx.coroutines.launch
 
@@ -28,7 +32,32 @@ fun Normal() {
     val snackBarHostState = remember { SnackbarHostState() }
     Scaffold(
         snackbarHost = {
-            SnackbarHost(hostState = snackBarHostState)
+            SnackbarHost(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                hostState = snackBarHostState
+            ) { data ->
+                Snackbar(
+                    action = {
+                        TextButton(
+                            onClick = { data.performAction() }
+                        ) {
+                            Text(data.visuals.actionLabel!!)
+                        }
+                    },
+                    dismissAction = {
+                        IconButton(
+                            onClick = { data.dismiss() }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = Icons.Filled.Close.name
+                            )
+                        }
+                    }
+                ) {
+                    Text(data.visuals.message)
+                }
+            }
         },
         floatingActionButton = {
             ExtendedFloatingActionButton(
